20f9b1201be7057969d7c1b8299dace91a23e34d
scriptsmy <78846415+scriptsmy@users.noreply.github.com>
2021-02-12 08:03:41 +0400

    Add files via upload

---
+++ js/up.js
@@ -0,0 +1,2 @@
+var smoothJumpUp=function(){if(document.body.scrollTop>0||document.documentElement.scrollTop>0){window.scrollBy(0,-50);setTimeout(smoothJumpUp,20);}}
+window.onscroll=function(){var scrolled=window.pageYOffset||document.documentElement.scrollTop;if(scrolled>100){document.getElementById('upbutton').style.display='block';}else{document.getElementById('upbutton').style.display = 'none';}}
